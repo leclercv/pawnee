f3e31ea3e4f617a0d4c61249f1e87329d9ee6b2b
Vincent Leclercq <vinz.62580@hotmail.fr>
2015-01-28 10:52:01 +0100

    code dans socket.c

--- webserver/socket.c
+++ webserver/socket.c
@@ -4,7 +4,21 @@ int creer_serveur (int port ) {
 
 	int socket_serveur ;
 
-	socket_serveur = socket ( AF_INET , SOCK_STREAM , 0);
+
+
+
+
+	 struct sockaddr_in saddr ;
+	saddr . sin_family = AF_INET ; /* Socket ipv4 */
+	saddr . sin_port = htons (8000); /* Port d ’ écoute */
+	saddr . sin_addr . s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
+
+
+
+	if(socket_serveur = socket ( AF_INET , SOCK_STREAM , 0)) == -1){
+		perror("socket serveur");
+		return -1;
+	}
 
 
 	if ( socket_serveur == -1){
@@ -13,10 +27,6 @@ int creer_serveur (int port ) {
 	/* Utilisation de la socket serveur */
 
 
-	 struct sockaddr_in saddr ;
-	saddr . sin_family = AF_INET ; /* Socket ipv4 */
-	saddr . sin_port = htons (8000); /* Port d ’ écoute */
-	saddr . sin_addr . s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
 	if ( bind ( socket_serveur , (struct sockaddr *)& saddr , sizeof( saddr )) == -1){
 		perror ( " bind socker_serveur " );
 	/* traitement de l ’ erreur */
@@ -26,5 +36,21 @@ int creer_serveur (int port ) {
 		perror ( " listen socket_serveur " );
 	/* traitement d ’ erreur */
 	}
+	const char * message_bienvenue = "Bonjour, bienvenue sur mon serveur!\n" ;
 
+	while(1){
+		int socket_client ;
+		socket_client = accept ( socket_serveur , NULL , NULL );
+		/* traitement d ’ erreur */
+		if ( socket_client == -1){
+			perror ("accept");
+		}
+		/* On peut maintenant dialoguer avec le client */
+		write (socket_client,message_bienvenue,strlen(message_bienvenue));
+
+		
+
+	}
+	}
+	return 0;
 }
